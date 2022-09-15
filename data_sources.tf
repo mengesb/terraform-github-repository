7f73b6acd153c050433668059ae87be2210c6a19
Brian Menges <mengesb@gmail.com>
2022-09-15 06:27:25 -0700

    update for github provider v5
    `data.github_repository` no longer has `branches` attribute. Adding `data.github_repository_branches` to get branches

--- data_sources.tf
+++ data_sources.tf
@@ -2,3 +2,13 @@
 # Terraform:: terraform-github-repository
 # Plan:: data_sources.tf
 #
+
+data "github_repository" "r" {
+  depends_on = [github_branch.b, github_branch_default.b]
+  full_name  = github_repository.r.full_name
+}
+
+data "github_repository_branches" "r" {
+  depends_on = [github_branch.b, github_branch_default.b]
+  repository = github_repository.r.name
+}
