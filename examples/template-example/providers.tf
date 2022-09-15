b88c602b9a8d2266e65f47e7c98140f82f5ab528
Brian Menges <mengesb@gmail.com>
2022-09-15 06:48:07 -0700

    Update example provider versions

--- examples/template-example/providers.tf
+++ examples/template-example/providers.tf
@@ -7,7 +7,7 @@ terraform {
   required_version = "~> 1.0"
   required_providers {
     github = {
-      version = "~> 4.0"
+      version = "~> 5.0"
       source  = "integrations/github"
     }
   }
